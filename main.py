4d68501dac3227f3f9e80c794558e20c11049f72
forza-png <codingal.sad@gmail.com>
2026-04-17 21:16:28 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Printing a message
+print("Hello, World!")
+
+# Printing my name
+print("My name is Rudransh.")
+
+#Printing a fun fact about myself
+#print("I love cars a lot and I am a huge fan of the Koenigsegg Jesko Absolut.")
+print("I am a huge fan of the Koenigsegg Jesko Absolut.")
+
+#Performing a simple calculation
+print("The sum of 99 and 1 is:", 99 + 1)
+
+#Performing another calculation
+print("the product of 12 and 123456789 is:", 12 * 123456789)
