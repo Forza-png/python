f16e85a1ee268492d1fd56ecacb195f18244e16d
forza-png <codingal.sad@gmail.com>
2026-04-29 21:02:00 +0530

    Average

---
+++ average.py
@@ -0,0 +1,14 @@
+# Storing Values
+car1 = 100
+car2 = 150
+car3 = 3321
+car4 = 21314
+car5 = 2345123
+
+#Finding the total cars
+sum = car1 + car2 + car3 + car4 + car5
+print("Total cars: ", sum)
+
+#Finding the average cars
+average = sum / 5
+print("Average cars: ", average)
