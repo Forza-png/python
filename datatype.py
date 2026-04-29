bc500be5cfaad07ca5b323294228b2ec152e8f5b
forza-png <codingal.sad@gmail.com>
2026-04-29 20:20:38 +0530

    ronaldo is the GOAT

---
+++ datatype.py
@@ -0,0 +1,41 @@
+username = input("What is your name? ")
+print(username)
+
+
+
+
+name  = "Blue"
+
+age = 1234567890
+
+is_student = True
+
+weight = 204.87
+
+print("Name:", name)
+
+print("Age:", age)
+
+print("Is Student:", is_student)
+
+print("Data Type of Age is:", type(age))
+
+print("is_student :", is_student)
+print("Data Type of is_student is:", type(is_student))
+
+print("weight :", weight)
+print("Data Type of weight is:", type(weight))
+
+print("\n After Type Casting....")
+
+age = str(age)
+
+print(age)
+
+print("Data Type of Age is:", type(age))
+
+weight = int(weight)
+
+print(weight)
+
+print("Data Type of weight is:", type(weight))
