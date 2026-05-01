9a0f55242aa631e2c5ec273e8c6857e353389d09
forza-png <codingal.sad@gmail.com>
2026-05-01 21:13:44 +0530

    comparision

---
+++ compare.py
@@ -0,0 +1,10 @@
+i = int(input("Enter a number: "))
+
+if (i < 15):
+     print("i'm in if block")
+     print("i is smaller than  15.")
+    
+else:
+    print("i is greater than  15.")
+    print("i'm in else block")
+print("i'm not in else and if block")
