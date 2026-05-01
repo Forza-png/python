c9e2726bba9604d29bc2ee439cafecfa40f5d2fc
forza-png <codingal.sad@gmail.com>
2026-05-01 21:24:16 +0530

    even odd

---
+++ even&odd.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number to check if it's even or odd: "))
+
+print("Number to be checked: ", number)
+
+if (number % 2) == 0:
+
+    print("This number is even.")
+
+else:
+   
+    print("This number is odd.")
