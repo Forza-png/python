a156af33101f25e52f1db877d7cf62a59ef08849
forza-png <codingal.sad@gmail.com>
2026-05-06 19:47:01 +0530

    n

--- even&odd.py
+++ even&odd.py
@@ -5,7 +5,6 @@ print("Number to be checked: ", number)
 if (number % 2) == 0:
 
     print("This number is even.")
-
 else:
-   
     print("This number is odd.")
+    
---
+++ gogoggogogogoggo.py
@@ -0,0 +1,11 @@
+# 1. Create a string variable
+text = "CONGRATS! Y0U WON A GOLD MEDAL!"
+
+# 2. Print the string
+print(text)  # Output: CONGRATS! Y0U WON A GOLD MEDAL!
+
+# 3. Get the length of the string
+print(len(text))  # Output: 14
+
+# 4. Convert to uppercase
+print(text.upper())  # Output: CONGRATS! Y0U WON A GOLD MEDAL!
