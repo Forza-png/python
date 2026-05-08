8e0a4439ee738c826dd5f478899740d689267256
forza-png <codingal.sad@gmail.com>
2026-05-08 21:03:52 +0530

    ear9gubq4a9

---
+++ percentage.py
@@ -0,0 +1,18 @@
+print("Enter the marks obtainted in 4 subjects: ")
+
+math = int(input("maths : "))
+
+science = int(input("science : "))
+
+english = int(input("english : "))
+
+hindi = int(input("hindi : "))
+
+sum = math+english+science+hindi
+
+print("sum of math,english,science and hindi = ", sum)
+
+perc = (sum/400)*100
+
+print(end = "percentage mark = ")
+print(perc)
