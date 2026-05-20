18c876c29c906288083ffce1ef2cd335a9a9fc8e
forza-png <codingal.sad@gmail.com>
2026-05-20 21:25:10 +0530

    annbruutururuturuut

---
+++ studentdetails.py
@@ -0,0 +1,31 @@
+name = input(" What is your name? ")
+
+print(name)
+
+age = input(" What is your age? ")
+
+print(age)
+
+grade = int(input("In which grade do you study? "))
+
+print(grade)
+
+live = input("where do you live? ")
+
+print(live)
+
+father_name = input("What is your father's name? ")
+
+print(father_name)
+
+mother_name = input("What is your mother's name? ")
+
+print(mother_name)
+
+contact_number = input("What is your contact number? ")
+
+print(contact_number)
+
+color = input("What is your favorite color? ")
+
+print(color)
