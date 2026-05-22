20109f5c7d7adb9c66445dc15b55d2793add5c11
forza-png <codingal.sad@gmail.com>
2026-05-22 21:21:27 +0530

    1234

---
+++ statements.py
@@ -0,0 +1,14 @@
+i = int(input("Enter a number : "))
+
+if (i < 15):
+
+    print ("i is smaller than 15")
+
+    print ("i am an in if Block")
+
+else:
+    print ("i is not smaller than 15")
+
+    print ("i'm in else Block")
+
+print ("i'm not in if and in else Block")
