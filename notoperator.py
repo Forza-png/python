32486967a8444d1e620d7d54c145788daa70b42f
forza-png <codingal.sad@gmail.com>
2026-05-27 21:12:00 +0530

    v

---
+++ notoperator.py
@@ -0,0 +1,33 @@
+a = 10
+
+b = 12
+
+c = 12
+
+# not is used here to reverse the result of (a == b)
+
+print(not (a == b))
+
+a = "python"
+
+b = "coding"
+
+#not is used here to check taht a is not equal to b
+
+if not (a == b):
+
+    print(a, 'and', b, 'are not equal')
+
+    a = 4
+
+    b = 5
+
+#not is used here to check that a is not greater than b
+
+if not((a == 1) == (b == 1)):
+
+    print("Hello, World!")
+
+    if not (a % 2 == 0):
+
+        print(a, "is an odd number")
