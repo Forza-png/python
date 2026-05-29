8232dd8384fc84ade50a164a915362902d276e54
forza-png <codingal.sad@gmail.com>
2026-05-29 21:12:18 +0530

    j

---
+++ bitwiseoperator.py
@@ -0,0 +1,15 @@
+a = 10
+
+b = -10
+
+# [rint bit right shift operator
+
+print("a >> 1 =", a >> 1)
+
+print("b >> 1 =", b >> 1)
+
+# print bit left shift operator
+
+print("a << 1 =", a << 1)
+
+print("b << 1=", b << 1)
